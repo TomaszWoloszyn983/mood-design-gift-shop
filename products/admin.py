5884657a5d2ac256af5e235558723763818e120e
Tomasz <tomaszwoloszyn983@gmail.com>
2023-01-15 10:47:06 +0000

    Create Category and Product classes and models

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,25 @@
 from django.contrib import admin
+from.models import Product, Category
 
 # Register your models here.
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'name',
+        'created_on',
+        'category',
+        'price',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+admin.site.register(Product)
+admin.site.register(Category)
