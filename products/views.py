a3bc695f3f49c44a207a8ec0c4fbce0fb777a1ac
Tomasz <tomaszwoloszyn983@gmail.com>
2023-01-16 19:06:17 +0000

    Style products panels. Add dropdown button

--- products/views.py
+++ products/views.py
@@ -1,9 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product
 
 def all_products(request):
     """
-    A view to show all prosucts,
+    A view to show all prosucts.
     """
     products = Product.objects.all()
 
@@ -11,3 +11,15 @@ def all_products(request):
         'products': products,
     }
     return render(request, 'products/products.html', context)
+
+
+def product_detail(request, product_id):
+    """
+    A view to show individual prosuct details.
+    """
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+    return render(request, 'product/product_detail.html', context)
