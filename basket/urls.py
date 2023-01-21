dfd1c2da38e214cf949b2f4ea326d05fd342d3ba
Tomasz <tomaszwoloszyn983@gmail.com>
2023-01-21 10:02:11 +0000

    Create add-to-basket function

--- basket/urls.py
+++ basket/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.show_basket, name='basket'),
+    path('add/<int:product_id>/', views.add_to_basket, name='add_to_basket'),
 ]
--- basket/views.py
+++ basket/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect, reverse
 
 def show_basket(request):
     """
@@ -16,6 +16,22 @@ def add_to_basket(request, product_id):
     Also it has to check if the product quantity is not already 0 
     to be able to add the product to the basket.
     """
+
+    print(f'Add some product by id {product_id} to the basket')
     basket = request.session.get('basket', {})
 
+    basket[product_id] = True
+
+    return redirect(reverse('basket'))
+
     # product = 
+
+# def delete_product(request, product_id):
+#     """
+#     A view to delete an existing product by its id.
+#     """
+#     product = get_object_or_404(Product, pk=product_id)
+#     product.delete()
+
+
+#     return redirect(reverse('products'))
