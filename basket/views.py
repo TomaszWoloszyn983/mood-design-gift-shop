c6037db155d69fdb2c911bbfa75d5aa7ea98711c
Tomasz <tomaszwoloszyn983@gmail.com>
2023-01-27 08:00:23 +0000

    Add ValueError handling to the add_to_basket function

--- basket/views.py
+++ basket/views.py
@@ -19,25 +19,27 @@ def add_to_basket(request, product_id):
     to be able to add the product to the basket.
     """
 
-    product = Product.objects.get(pk=product_id)
-    print(f'Add some product by id {product_id} to the basket')
-    units = int(request.POST.get('units'))
-    basket = request.session.get('basket', {})
-    print(f'Basket: {basket}')
 
-    if product_id in list(basket.keys()):
-        basket[product_id] += units
-    else:
-        basket[product_id] = units
-        if units > 1:
-            messages.success(request, f'Added {units} {product.name}s to your shopping basket')
+    try:
+        product = Product.objects.get(pk=product_id)
+        print(f'Add some product by id {product_id} to the basket')
+        units = int(request.POST.get('units'))
+        basket = request.session.get('basket', {})
+        print(f'Basket: {basket}')
+        
+        if product_id in list(basket.keys()):
+            basket[product_id] += units
         else:
-            messages.success(request, f'Added {units} {product.name} to your shopping basket')
+            basket[product_id] = units
+            if units > 1:
+                messages.success(request, f'Added {units} {product.name}s to your shopping basket')
+            else:
+                messages.success(request, f'Added {units} {product.name} to your shopping basket')
+        request.session['basket'] = basket
+        print(request.session['basket'])
+    except ValueError:
+        messages.error(request, f'Write a correct number')
             
-
-    request.session['basket'] = basket
-    print(request.session['basket'])
-
     return redirect(reverse('basket'))
 
 
