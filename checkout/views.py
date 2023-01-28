19afbe1abf1a6db13c9de683636676086b8e482c
Tomasz <tomaszwoloszyn983@gmail.com>
2023-01-28 18:13:30 +0000

    Add payment card input diagram into the checkout template

--- checkout/views.py
+++ checkout/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.contrib import messages
 from .forms import OrderForm
+from basket.contexts import basket_contents
 
 # Create your views here.
 def checkout(request):
