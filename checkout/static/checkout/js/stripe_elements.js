85e534bbc6f518eea575dc2f3dd0cc5e183a22a6
Tomasz <tomaszwoloszyn983@gmail.com>
2023-01-29 07:30:27 +0000

    Add basic payment functionality

--- checkout/static/checkout/js/stripe_elements.js
+++ checkout/static/checkout/js/stripe_elements.js
@@ -5,10 +5,10 @@
     https://stripe.com/docs/stripe-js
 */
 
-let stripe_public_key = $('#id_stripe_public_key').text().slice(1, -1);
+let stripePublicKey = $('#id_stripe_public_key').text().slice(1, -1);
 let clientSecret = $('#id_client_secret').text().slice(1, -1);
-let stripe = Stripe(stripe_public_key);
-let elements = stripe.elements(); 
+let stripe = Stripe(stripePublicKey);
+let elements = stripe.elements();
 let style = {
     base: {
         color: '#000',
@@ -24,12 +24,12 @@ let style = {
         iconColor: '#dc3545'
     }
 };
-
 let card = elements.create('card', {style: style});
 card.mount('#card-element');
 
+
 // Handle realtime validation errors on the card element
-card.addEventListener('change', function (event) {
+card.addEventListener('change', function (event){
     let errorDiv = document.getElementById('card-errors');
     if (event.error) {
         let html = `
@@ -118,3 +118,28 @@ form.addEventListener('submit', function(ev) {
     })
 });
 
+// const form = document.getElementById('payment-form');
+
+// form.addEventListener('submit', async (event) => {
+//   event.preventDefault();
+
+//   const {error} = await stripe.confirmPayment({
+//     //`Elements` instance that was used to create the Payment Element
+//     elements,
+//     confirmParams: {
+//       return_url: 'https://example.com/order/123/complete',
+//     },
+//   });
+
+//   if (error) {
+//     // This point will only be reached if there is an immediate error when
+//     // confirming the payment. Show error to your customer (for example, payment
+//     // details incomplete)
+//     const messageContainer = document.querySelector('#error-message');
+//     messageContainer.textContent = error.message;
+//   } else {
+//     // Your customer will be redirected to your `return_url`. For some payment
+//     // methods like iDEAL, your customer will be redirected to an intermediate
+//     // site first to authorize the payment, then redirected to the `return_url`.
+//   }
+// });
--- checkout/views.py
+++ checkout/views.py
@@ -1,27 +1,40 @@
 from django.shortcuts import render
 from django.contrib import messages
-from django.contrib import settings
+from django.conf import settings
 from .forms import OrderForm
 from basket.contexts import basket_contents
 import stripe
 
 # Create your views here.
 def checkout(request):
+
+    stripe_public_key = settings.STRIPE_PUBLIC_KEY
+    stripe_secret_key = settings.STRIPE_SECRET_KEY
+
     basket = request.session.get('basket', {})
     if not basket:
         messages.error(request, 'The basket is empty')
         return redirect(reverse('products'))
 
-    current_bag = bag_contents(request)
-    total = current_bag['grand_total']
+    current_basket = basket_contents(request)
+    total = current_basket['grand_total']
     stripe_total = round(total * 100)
+    stripe.api_key = stripe_secret_key
+    intent = stripe.PaymentIntent.create(
+        amount=stripe_total,
+        currency=settings.STRIPE_CURRENCY,
+    )
+
+    if not stripe_public_key:
+        messages.warning(request, 'Stripe public key is missing. \
+            Set your Stripe public key in your environment?')
 
     order_form = OrderForm()
     template = 'checkout/checkout.html'
     context = {
         'order_form': order_form,
         'stripe_public_key': 'pk_test_51MFBRILWWeH2Y5WlTvWhOsM4yRoTrAo6FaQQeTuypLAJe8B0tZUKOyFVLIHmb1xFl3V2jqOo4FLCJnr9zOqSfZKb00PrYaYpUv',
-        'client_secret': 'test client secret',
+        'client_secret': 'intent.client secret',
     }
 
     return render(request, template, context)
