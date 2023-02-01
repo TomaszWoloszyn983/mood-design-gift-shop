e8875a0eeca2abc92c185310a0d3251609176257
Tomasz <tomaszwoloszyn983@gmail.com>
2023-02-01 08:34:02 +0000

    Edit Profile Page. Display profile form and order history.

--- profiles/views.py
+++ profiles/views.py
@@ -1,11 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from .models import UserProfile
+from .forms import UserProfileForm
+from checkout.models import Order
+
 
 def profile(request):
     """
     Display users profile.
+
     """
+    profile = get_object_or_404(UserProfile, user=request.user)
+
+    if request.method == 'POST':
+        form = UserProfileForm(request.POST, instance=profile)
+        if form.is_valid():
+            form.save()
+            messages.success(request, 'Profile data saved')
+        else:
+            messages.error(request,
+                           ('Some of yout input data are not valid.'
+                           'Please try again'))
+    else:
+        form = UserProfileForm(instance=profile)
+    orders = profile.orders.all()
 
     context = {
-
+        'profile': profile,
+        'form': form,
+        'orders': orders,
     }
     return render(request, 'profiles/profile.html', context)
