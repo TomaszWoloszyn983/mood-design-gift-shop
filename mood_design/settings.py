e3692e4e8555e62c09bbcc6f6a604fa6d5addb97
Tomasz <tomaszwoloszyn983@gmail.com>
2023-02-04 09:24:17 +0000

    Deploy to Heroku

--- mood_design/settings.py
+++ mood_design/settings.py
@@ -10,8 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/3.2/ref/settings/
 """
 
-from pathlib import Path
 import os
+import dj_database_url
+from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -21,12 +22,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-$s+8e@z7om2tdj3m6c7n6a+e+0^iw3b0!_d@r2=y+m(&20t&+1'
+# SECRET_KEY = 'django-insecure-$s+8e@z7om2tdj3m6c7n6a+e+0^iw3b0!_d@r2=y+m(&20t&+1'
+SECRET_KEY =  os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mood-design-gift-shop.herokuapp.com', 'localhost']
 
 
 # Application definition
@@ -117,12 +119,20 @@ WSGI_APPLICATION = 'mood_design.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+if 'DATABASE_URL' in os.environ:
+    # Heroku Elephant database
+    DATABASES = {
+    'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
     }
-}
+else:
+    # Local database
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
+
 
 
 # Password validation
