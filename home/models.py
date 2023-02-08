934965ff4622cc671ffd5722f56df5179668ccd6
Tomasz <tomaszwoloszyn983@gmail.com>
2023-02-08 19:03:52 +0000

    Create NewsletterUser and Post models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,29 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+
+class NewsletterUser(models.Model):
+    email = models.EmailField(max_length=254, null=False, blank=False)
+    added_on = models.DateTimeField(auto_now_add=True)
+
+
+class Post(models.Model):
+    DESIGNERS = [
+        ('Ela', 'Ela'),
+        ('Lukasz', 'Lukasz')
+    ]
+
+    title = models.TextField()
+    designer = models.CharField(choices=DESIGNERS, max_length=10, default='Ela')
+    created_on = models.DateTimeField(auto_now_add=True)
+    updated_on = models.DateTimeField(auto_now=True)
+    body = models.TextField()
+    image = models.ImageField(null=True, blank=True)
+    email = models.ManyToManyField(NewsletterUser)
+
+    class Meta:
+        ordering = ["-created_on"]
+
+    def __str__(self):
+        return self.title
