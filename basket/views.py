e6de78630c3895831b103f2f00f626fa471b2dc1
Tomasz <tomaszwoloszyn983@gmail.com>
2023-02-17 19:39:49 +0000

    Improve layouts in Shop, workshop and basket pages

--- basket/views.py
+++ basket/views.py
@@ -13,10 +13,6 @@ def add_to_basket(request, product_id):
     """
     A view to add product to the shopping basket.
 
-    It's planned to add product quantity variable to the product model
-    This value would be updated in this function.
-    Also it has to check if the product quantity is not already 0 
-    to be able to add the product to the basket.
     """
 
 
