1c8fce47910fac896fe053a6cd664017111a63e8
Tomasz <tomaszwoloszyn983@gmail.com>
2023-02-20 15:31:04 +0000

    Change debug to false

--- mood_design/settings.py
+++ mood_design/settings.py
@@ -27,7 +27,7 @@ SECRET_KEY =  os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 # DEBUG = 'DEVELOPMENT' in os.environ
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['mood-design-gift-shop.herokuapp.com', 'localhost']
 
